6dfb7d56c2e2b0e52f1ce1f8aedb257e7ce0f479
QwertyIsCoding <pranav.pyari@gmail.com>
2025-11-26 19:58:31 -0500

    good swerves

--- PhoenixSwerve/src/main/java/frc/robot/RobotContainer.java
+++ PhoenixSwerve/src/main/java/frc/robot/RobotContainer.java
@@ -16,11 +16,12 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 
-import frc.robot.generated.TunerConstants;
+import frc.robot.generated.TunerConstantsDragon;
+import frc.robot.generated.TunerConstantsRuby;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 
 public class RobotContainer {
-    private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
+    private double MaxSpeed = TunerConstantsDragon.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
     private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
 
     /* Setting up bindings for necessary control of the swerve drive platform */
@@ -34,7 +35,7 @@ public class RobotContainer {
 
     private final CommandXboxController joystick = new CommandXboxController(0);
 
-    public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
+    public final CommandSwerveDrivetrain drivetrain = TunerConstantsDragon.createDrivetrain();
 
     public RobotContainer() {
         configureBindings();
